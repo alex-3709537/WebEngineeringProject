23f7f725afcc64ad810a1a8354618c5c4a888a85
Alex_3709537 <alex.fleig@online.de>
2024-05-21 13:13:25 +0200

    Port auf 8080 geändert

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 4000;
+const port = 8080;
 
 const login = require("./routes/login");
 const user = require("./routes/user");
