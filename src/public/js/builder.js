f72964b8085f4615e2c3a21e13d45f1548966dc2
Alex_3709537 <alex.fleig@online.de>
2024-06-17 18:55:31 +0200

    Datum beim Post hinzugefügt

--- src/public/js/builder.js
+++ src/public/js/builder.js
@@ -9,14 +9,22 @@ export function getPostContainer(username, post) {
         const divUsername = document.createElement("div");
         const divMedia = document.createElement("div");
         const divText = document.createElement("div");
+        const divHeader = document.createElement("div");
         const p = document.createElement("p");
+        const divDate = document.createElement("div");
 
+        divHeader.setAttribute("class", "flex-container-post-header");
+        divDate.setAttribute("class", "flex-container-post-date");
         divMain.setAttribute("class", "flex-container-post");
-        divUsername.setAttribute("class", "flex-container-post-header");
+        divUsername.setAttribute("class", "flex-container-post-username");
         divText.setAttribute("class", "flex-cantainer-post-input");
 
         divUsername.innerHTML = username;
-        divMain.appendChild(divUsername);
+        divHeader.appendChild(divUsername);
+
+        divDate.innerHTML = getDate(post.date);
+        divHeader.appendChild(divDate);
+        divMain.appendChild(divHeader);
 
         if (post.data != undefined && post.data != "unknown-Content-Type") {
             divMedia.appendChild(post.data);
@@ -30,5 +38,43 @@ export function getPostContainer(username, post) {
         return divMain;
     } catch (err) {
         console.log(err.message);
+    }
+}
+
+function getDate(date){
+    const d = new Date(date);
+    const dn = new Date();
+
+    const timeDiff = dn.getTime() - d.getTime();
+    var timeString;
+
+    if((timeDiff / 1000) < 60){
+        timeString = Math.floor(timeDiff / 1000) + " seks"
+    }else if((timeDiff / 1000)/60 < 60 ){
+        timeString = Math.floor((timeDiff / 1000)/60) + " mins"
+    }else if(((timeDiff / 1000)/60)/60 < 24){
+        timeString = Math.floor(((timeDiff / 1000)/60)/60) + " hrs"
+    }else if((((timeDiff / 1000)/60)/60)/24 < 7){
+        timeString = Math.floor((((timeDiff / 1000)/60)/60)/24) + " days"
+    }else if(((((timeDiff / 1000)/60)/60)/24)/7 < 4){
+        timeString = Math.floor(((((timeDiff / 1000)/60)/60)/24)/7) + " weeks"
+    }else if((((((timeDiff / 1000)/60)/60)/24)/7)/4 < 12){
+        timeString = Math.floor((((((timeDiff / 1000)/60)/60)/24)/7)/4) + " months"
+    }else{
+        timeString = Math.floor(((((((timeDiff / 1000)/60)/60)/24)/7)/4)/12) + " years"
+    }
+
+    
+    //const dateString = `${hasZero(d.getDate())}.${hasZero(d.getMonth())}.${hasZero(d.getFullYear())} ${hasZero(d.getHours())}:${hasZero(d.getMinutes())}`;
+    return timeString;
+
+
+
+    function hasZero(date){
+        if(date < 10){
+            return "0" + date;
+        }else{
+            return date;
+        }
     }
 }
