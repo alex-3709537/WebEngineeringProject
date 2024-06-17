3ea155c6b3962963211baf078bb8abd3304d2ddb
unlqvt <lscharnweber05@gmail.com>
2024-06-17 14:14:42 +0200

    Leere Posts können fortan nicht mehr gesendet werden, dann wird eine Auuforderung zum Setzen von Text angezeigt (deckt undefined, null, ", whitespace ab; erlaubt, wenn dateianhang ausgewählt)

--- src/public/js/uploadPost.js
+++ src/public/js/uploadPost.js
@@ -9,16 +9,22 @@ const postField = document.getElementById("post-text-field");
 document.getElementById("post-form").addEventListener("submit", async function(event) {
   event.preventDefault(); // Verhindert das Standardverhalten (Seiten-Reload);
   const form = event.target;
-  
 
-  const result = await setPost(form);
+  if ((postField.value === undefined || postField.value === null || postField.value === "" || postField.value.trim() === "" || postField.value === "Enter a text to post!") && document.getElementById("file-input").value === "")
+  {
+    postField.value = "Enter a text to post!";
+  }
+  else
+  {
+    const result = await setPost(form);
 
-  postField.value = "";
-  document.getElementById("file-input").value = "";
-  
-  const post = await getFullPost(result.pid);
-  const div = getPostContainer(result.username, post);
-  document.getElementById("post-field").append(div);
+    postField.value = "";
+    document.getElementById("file-input").value = "";
+      
+    const post = await getFullPost(result.pid);
+    const div = getPostContainer(result.username, post);
+    document.getElementById("post-field").append(div);
+  }
   
 });
 
